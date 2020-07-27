69cea3f1e011b9c7d380697828a6256e28e39e8f
EmeHomes <mscasas.94@gmail.com>
2020-07-27 22:37:20 +0200

    - Basic auth - Restful to JPA

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER values (1, sysdate(), 'Doremi');
+INSERT INTO USER values (2, sysdate(), 'Fa');
+INSERT INTO USER values (3, sysdate(), 'Solar');
+INSERT INTO USER values (4, sysdate(), 'Sid');
